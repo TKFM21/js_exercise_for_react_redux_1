5cbc5bdb8e8a166a18fc68bcdae0550ef02a004e
TKFM21 <tkzw21@gmail.com>
2019-11-21 17:49:34 +0900

    add toggle function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { toggleTodoCompleted } from '../actions/todoActionCreator';
 
 const TodoList = (props) => {
     const todoItems = props.todos.map( (todo, index) => {
         return (
             <div key={index}>
-                <p>{todo.text}</p>
+                <span>{todo.text}</span>
+                <button onClick={ () => props.toggleTodoCompleted(index) }>
+                    { todo.hasCompleted() ? '完了' : '未完了' }
+                </button>
             </div>
         );
     });
@@ -20,6 +24,16 @@ const mapStateToProps = (state) => {
     return { todos: state.todos };
 };
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        toggleTodoCompleted: (index) => {
+            const toggleAction = toggleTodoCompleted(index);
+            dispatch(toggleAction);
+        }
+    };
+};
+
 export default connect(
-    mapStateToProps
+    mapStateToProps,
+    mapDispatchToProps
 )(TodoList);
