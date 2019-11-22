57c0fef711a1c3cb3801c821cf3a97df228049b8
Fumiaki Takizawa <tkzwfm21@gmail.com>
2019-11-22 18:43:41 +0900

    add visibleFilter function

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { deleteTodo, toggleTodoCompleted } from '../actions/todoActionCreator';
+import {
+    SHOW_ALL,
+    SHOW_ACTIVE,
+    SHOW_COMPLETED
+} from '../actions/visibleFilterActionCreator';
 
 const TodoList = (props) => {
-    const todoItems = props.todos.map( (todo, index) => {
+    const visibleFilteredTodoItems = props.todos.filter( todo => {
+        switch (props.visibleFilter) {
+            case SHOW_ALL:
+                return true;
+            case SHOW_ACTIVE:
+                return !todo.hasCompleted();
+            case SHOW_COMPLETED:
+                return todo.hasCompleted();
+            default:
+                return false;
+        }
+    });
+    const todoItems = visibleFilteredTodoItems.map( (todo, index) => {
         return (
             <div key={index}>
                 <span>{todo.text}</span>
@@ -18,13 +35,16 @@ const TodoList = (props) => {
     });
     return (
         <div>
-            {todoItems}
+            { todoItems }
         </div>
     );
 };
 
 const mapStateToProps = (state) => {
-    return { todos: state.todos };
+    return {
+        todos: state.todos,
+        visibleFilter: state.visibleFilter
+    };
 };
 
 const mapDispatchToProps = (dispatch) => {
